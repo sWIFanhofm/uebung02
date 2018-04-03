a4995f31717f0d799a02bcd0ca390ca235b62f93
Andi Hofmann <andreas-h-online@web.de>
2018-04-03 21:23:09 +0200

    Test

--- src/main/java/ueb02/Duplikate.java
+++ src/main/java/ueb02/Duplikate.java
@@ -5,7 +5,7 @@ class Duplikate {
 	 * Gibt ein StringSet mit den Wörtern zurück, welche mindestens zwei mal im Text vorkommen.
 	 * Alle Satzzeichen im Text sollen ignoriert werden.
 	 * @param text Eingabetext, kann Satzzeichen enthalten welche ignoriert werden.
-	 * @return StringSet mit den Wörtern, welche mind. zwei mal vorkommen.
+	 * @return StringSet mit den Wörtern, welche mind. zwei mal vorkommen..
 	 */
 	static StringSet findeDuplikate(String text) {
 		// TODO Implementieren Sie die Methode gemäß dem obigen Javadoc Kommentar.
